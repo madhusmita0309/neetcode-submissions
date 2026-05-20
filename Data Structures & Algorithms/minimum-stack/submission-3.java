e45be68171f16750f9491ceb4ada6118c44723a3
Madhusmita Oke <43229624+madhusmita0309@users.noreply.github.com>
2026-05-20 15:58:11 -0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,38 @@
+class MinStack {
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new ArrayDeque<>();
+        this.minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        this.stack.push(val);
+        
+
+        if(this.minStack.isEmpty() ||  val <= this.minStack.peek())
+            this.minStack.push(val);
+       
+    }
+    
+    public void pop() {
+        if(this.stack.isEmpty())
+            return;
+        int top = this.stack.pop();
+        if(this.minStack.peek() == top)
+            this.minStack.pop();
+        
+        
+        
+    }
+    
+    public int top() {
+        return this.stack.peek();
+    }
+    
+    public int getMin() {
+      
+        return this.minStack.peek();
+    }
+}
